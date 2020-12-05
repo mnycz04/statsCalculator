8be76b0f380f3ec673a25f128112e66402be40ce
mnycz <mnycz04@gmail.com>
2020-12-04 23:33:53 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+# A Calculator for common AP Stats Problems
+"""
+
+__author__ = 'Michael Nycz'
+__version__ = '1.0'
+
+
+while 1:
+    points = input("Enter the number of events: ")
+    try:
+        points = int(points)
+        if points <=0:
+            raise ValueError
+    except ValueError:
+        print("Please enter a integer number, greater than 0:")
+
+    events_base = []
+
+    for i in range(points):
+
